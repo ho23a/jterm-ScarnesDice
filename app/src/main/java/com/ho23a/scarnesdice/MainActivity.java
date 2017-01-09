4dbcfb8b1bdc8cf8af9e59d945bc4e52cdb4e2fc
Anh Ho <ph.anh.ho@gmail.com>
2017-01-08 22:53:52 -0500

    Basic Scarne's Dice

--- app/src/main/java/com/ho23a/scarnesdice/MainActivity.java
+++ app/src/main/java/com/ho23a/scarnesdice/MainActivity.java
@@ -2,12 +2,137 @@ package com.ho23a.scarnesdice;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int MAX_ROLL = 6;
+    private static final int WIN_SCORE = 20;
+    private int playerTotal;
+    private int computerTotal;
+    private int currentTurn;
+    private Players whosTurn;
+    private Random random = new Random();
+    private Toast toast;
+
+    enum Players {
+        PLAYER,
+        COMPUTER,
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        playerTotal = 0;
+        computerTotal = 0;
+        currentTurn = 0;
+        whosTurn = Players.PLAYER;
+        String message = "It's Your Turn";
+        toast = Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT);
+        toast.show();
+
+        ((Button) findViewById(R.id.rollButton)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                roll();
+            }
+        });
+
+        ((Button) findViewById(R.id.holdButton)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                hold();
+            }
+        });
+
+        ((Button) findViewById(R.id.resetButton)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                reset();
+            }
+        });
+    }
+
+    public void roll() {
+        int rolled = rollDice();
+
+        String message = String.format("%s rolled a %d", whosTurn.toString(), rolled);
+        toast = Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT);
+        toast.show();
+
+        if (rolled == 1) {
+            changePlayers();
+        } else {
+            currentTurn += rolled;
+        }
+    }
+
+    public void hold() {
+        if (whosTurn == Players.PLAYER) {
+            addPlayerScore(currentTurn);
+        } else {
+            addComputerScore(currentTurn);
+        }
+    }
+
+    public void reset() {
+        resetScore();
+    }
+
+    private int rollDice() {
+        return random.nextInt(MAX_ROLL) + 1;
+    }
+
+    private void changePlayers() {
+        currentTurn = 0;
+
+        String message = "";
+        if (whosTurn == Players.PLAYER) {
+            whosTurn = Players.COMPUTER;
+            message = "It's Computer's Turn";
+        } else {
+            whosTurn = Players.PLAYER;
+            message = "It's Your Turn";
+        }
+        toast = Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    private void addPlayerScore(int currentTurn) {
+        playerTotal += currentTurn;
+        ((EditText) findViewById(R.id.playerScoreText)).setText(Integer.toString(playerTotal), TextView.BufferType.EDITABLE);
+        if (playerTotal >= WIN_SCORE) {
+            toast = Toast.makeText(getApplicationContext(), "You Won!", Toast.LENGTH_SHORT);
+            toast.show();
+        } else {
+            changePlayers();
+        }
+    }
+
+    private void addComputerScore(int currentTurn) {
+        computerTotal += currentTurn;
+        ((EditText) findViewById(R.id.computerScoreText)).setText(Integer.toString(computerTotal), TextView.BufferType.EDITABLE);
+        if (computerTotal >= WIN_SCORE) {
+            toast = Toast.makeText(getApplicationContext(), "Sorry, you lost! Computer Won!", Toast.LENGTH_SHORT);
+            toast.show();
+        } else {
+            changePlayers();
+        }
+    }
+
+    private void resetScore() {
+        playerTotal = 0;
+        computerTotal = 0;
+        whosTurn = Players.PLAYER;
+        currentTurn = 0;
+        ((EditText) findViewById(R.id.playerScoreText)).setText(Integer.toString(playerTotal), TextView.BufferType.EDITABLE);
+        ((EditText) findViewById(R.id.computerScoreText)).setText(Integer.toString(computerTotal), TextView.BufferType.EDITABLE);
     }
 }
